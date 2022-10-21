eaf2528459b9241cb381dcbbd3e3283c57d06460
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2022-10-21 11:06:08 +0300

    Time: 171 ms (5.16%), Space: 20.1 MB (9.56%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.py
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -10,12 +10,12 @@ class Solution:
         if not node :
             return 0
         if not vis:
-            move1 = self.dfs(node.left,False)
+            move1 = self.dfs(node.left)
             move2 = self.dfs(node.left,True)
-            move3 = self.dfs(node.right,False)
+            move3 = self.dfs(node.right)
             move4 = self.dfs(node.right,True)
             return max(move1+move3,move1+move4,move2+move3,move2+move4)
-        return node.val + self.dfs(node.left,False) + self.dfs(node.right, False)
+        return node.val + self.dfs(node.left) + self.dfs(node.right)
     def rob(self, root: Optional[TreeNode]) -> int:
         return max(self.dfs(root),self.dfs(root,True))
                 
