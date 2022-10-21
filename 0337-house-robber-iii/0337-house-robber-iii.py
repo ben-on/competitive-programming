acc793e1c428707e7bb301970c440e88f8b84276
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2022-10-21 10:05:25 +0300

    Time: 103 ms (43.56%), Space: 18.7 MB (9.56%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.py
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -6,17 +6,21 @@
 #         self.right = right
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
-        @cache
+        dp={}
         def dfs(node,vis = False):
             if not node :
                 return 0
+            if (node,vis) in dp:
+                return dp[(node,vis)]
             if not vis:
                 m1 = dfs(node.left,False)
                 m2 = dfs(node.left,True)
                 m3 = dfs(node.right,False)
                 m4 = dfs(node.right,True)
-                return max(m1+m3,m1+m4,m2+m3,m2+m4)
-            return node.val + dfs(node.left,False) + dfs(node.right, False)
+                dp[(node,vis)] = max(m1+m3,m1+m4,m2+m3,m2+m4)
+            else:
+                dp[(node,vis)] = node.val + dfs(node.left,False) + dfs(node.right, False)
+            return dp[(node,vis)]
         return max(dfs(root),dfs(root,True))
                 
             
