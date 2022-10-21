b61da24a42826ba7e5b21d0fc7a4469ed91f13f9
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2022-10-21 21:31:10 +0300

    Time: 3318 ms (5.00%), Space: 143.1 MB (5.24%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,10 +1,12 @@
 class Solution:
+    t1,t2=None,None
+    @cache
+    def dynamic(self,i,j):
+        if i >= len(self.t1) or j >= len(self.t2):
+            return 0
+        if self.t1[i] == self.t2[j]:
+            return 1 + self.dynamic(i+1, j+1)
+        return max(self.dynamic(i+1,j), self.dynamic(i,j+1))
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        @cache
-        def dynamic(i,j):
-            if i >= len(text1) or j >= len(text2):
-                return 0
-            if text1[i] == text2[j]:
-                return 1 + dynamic(i+1, j+1)
-            return max(dynamic(i+1,j), dynamic(i,j+1))
-        return dynamic(0,0)
+        self.t1,self.t2 = text1, text2
+        return self.dynamic(0,0)
