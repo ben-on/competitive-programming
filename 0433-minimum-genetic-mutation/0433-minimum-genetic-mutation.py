596fcd7db0a1b9197981e0cb0089d59e0604bc3a
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2022-11-02 22:53:25 +0300

    Time: 59 ms (38.38%), Space: 14 MB (37.03%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -1,28 +1,20 @@
 class Solution:
     def minMutation(self, start: str, end: str, bank: List[str]) -> int:
         bank = set(bank)
-        errors = []
-        for i in range(8):
-            if start[i] != end[i]:
-                errors.append((i))
         que = deque([start])
-        # return len(errors)
         d = 0
         vis = set()
         vis.add(start)
-        while que :
+        while que:
             r = len(que)
-            # print(que)
             for _ in range(r):
                 temp = que.popleft()
-                
                 if temp == end :
                     return d
                 for nb in bank:
                     if nb not in vis:
                         b = 0
                         for i in range(8):
-                            # print(nb)
                             if nb[i] != temp[i]:
                                 b+=1
                             if b>1:
@@ -30,7 +22,6 @@ class Solution:
                         if b ==1:
                             que.append(nb)
                             vis.add(nb)
-            d+=1
-                    
+            d+=1       
         return -1
         
