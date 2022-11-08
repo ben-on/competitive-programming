3338728567d2b6c5710bb12439b49de69199dd0c
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2022-11-08 22:25:38 +0300

    Time: 105 ms (5.13%), Space: 14 MB (15.06%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -0,0 +1,10 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stk = []
+        for i in s:
+            if stk and stk[-1] != i and stk[-1].lower() == i.lower():
+                stk.pop()
+            else:
+                stk.append(i)
+            print(stk)
+        return "".join(stk)
