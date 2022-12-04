b5fbf17f57c03a2ae302b6ba33c81e2de1890b8f
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2022-12-04 09:58:42 +0300

    Time: 2338 ms (60.63%), Space: 30.3 MB (8.58%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.py
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -6,13 +6,11 @@ class Solution:
         for i in range(1,n):
             pre.append(pre[-1]+nums[i])
             post.appendleft(post[0]+nums[-(i+1)])
-        # print(pre,post)
         ans = float('inf')
         ansI = None
         for i in range(n):
             avpre = pre[i]//(i+1)
             avpost = post[i+1]//(n-i-1) if i !=n-1 else 0
-            # print(avpre,avpost,avpre-avpost)
             tar = abs(avpre-avpost)
             if ans > tar:
                 ans = tar
