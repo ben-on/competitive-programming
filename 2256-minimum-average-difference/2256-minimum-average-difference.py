539b5eb0be1f16b922796e22e84b29ebab91fb1c
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2022-12-04 10:07:10 +0300

    Time: 2480 ms (52.05%), Space: 24.9 MB (56.92%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.py
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -1,18 +1,21 @@
+# time and space analysis
+# time: O(n)
+# space: O(n)
 class Solution:
     def minimumAverageDifference(self, nums: List[int]) -> int:
-        pre = [nums[0]]
-        post = deque([nums[-1]])
+        pre = 0
+        tot = sum(nums)
         n = len(nums)
-        for i in range(1,n):
-            pre.append(pre[-1]+nums[i])
-            post.appendleft(post[0]+nums[-(i+1)])
+        
         ans = float('inf')
         ansI = None
         for i in range(n):
-            avpre = pre[i]//(i+1)
-            avpost = post[i+1]//(n-i-1) if i !=n-1 else 0
+            pre += nums[i]
+            avpre = pre//(i+1)
+            avpost = (tot-pre)//(n-i-1) if i<n-1 else 0
             tar = abs(avpre-avpost)
-            if ans > tar:
+            if tar < ans:
                 ans = tar
                 ansI = i
         return ansI
+    
