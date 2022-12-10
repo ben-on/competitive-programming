30e4a4ee1d9eb2200e68e825c9bff8f777ce3af6
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2022-12-10 23:15:40 +0300

    Time: 1985 ms (76.88%), Space: 113.6 MB (65.05%) - LeetHub

---
+++ 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.py
@@ -0,0 +1,46 @@
+# class TreeNode:
+#     def __init__ (self,val=0,right=None,left=None):
+#         self.val = val
+#         self.right = right
+#         self.left = left
+
+        
+class Solution:
+    def countHighestScoreNodes(self, parents: List[int]) -> int:
+        graph = defaultdict(list)
+        for child,parent in enumerate(parents):
+            graph[parent].append(child)
+        print(graph)
+        
+        dp = {}
+        def dfs(node):
+            if not graph[node]:
+                dp[node] = 1
+                return 1
+            temp = 1
+            for nb in graph[node]:
+                temp += dfs(nb)
+            dp[node] = temp
+            return dp[node]
+        
+        tot = dfs(graph[-1][0])
+        print(dp)
+        # print(tot)
+        ans = -float('inf')
+        freq = None
+        for node in dp:
+            if graph[node]:
+                score = prod([dp[child] for child in graph[node]])
+                left = tot - (sum([dp[child] for child in graph[node]])+1)
+                if left:
+                    score *= left
+            else:
+                score = tot-1
+            if score > ans:
+                ans = score
+                freq = 1
+            elif score == ans:
+                freq +=1
+        return freq
+            
+                
