907151a40e0394f0bfdc94ba55ab042bce0784c0
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2022-12-15 11:12:17 +0300

    Time: 915 ms (66.06%), Space: 150.3 MB (5.00%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,12 +1,32 @@
 class Solution:
-    t1,t2=None,None
-    @cache
-    def dynamic(self,i,j):
-        if i >= len(self.t1) or j >= len(self.t2):
-            return 0
-        if self.t1[i] == self.t2[j]:
-            return 1 + self.dynamic(i+1, j+1)
-        return max(self.dynamic(i+1,j), self.dynamic(i,j+1))
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        self.t1,self.t2 = text1, text2
-        return self.dynamic(0,0)
+        n,m = len(text1),len(text2)
+        @cache
+        def dfs(i,j):
+            if i >= n or j >= m:
+                return 0
+            if text1[i] == text2[j]:
+                return max(1+dfs(i+1,j+1),dfs(i+1,j),dfs(i,j+1))
+            else:
+                return max(dfs(i+1,j),dfs(i,j+1))
+        return dfs(0,0)
+        # # the tabulation method
+        # dp = [[0 for i in text1] for j in text2]
+        # cur = 0
+        # n,m = len(text2),len(text1)
+        # for i in range(m):
+        #     if text2[0] == text1[i]:
+        #         cur += 1
+        #     dp[0][i] = cur
+        # cur = 0
+        # for i in range(n):
+        #     if text1[0] == text2[i]:
+        #         cur += 1
+        #     dp[i][0] = cur
+        # for i in range(1,n):
+        #     for j in range(1,m):
+        #         if text1[j] == text2[i]:
+        #             dp[i][j] = 1+dp[i-1][j-1]
+        #         else:
+        #             dp[i][j] = dp[i-1][j-1]
+        # return max(dp[-1])
