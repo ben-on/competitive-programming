7f75d5c24dbc733df52f065d4ca59b111636b5b2
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2022-12-27 23:50:32 +0300

    Time: 988 ms (80.73%), Space: 22.1 MB (88.04%) - LeetHub

---
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
+        n = len(rocks)
+        track, count = [], 0
+        for i in range(n):
+            diff = capacity[i] - rocks[i]
+            heappush(track, diff)
+        for i in range(n):
+            additionalRocks -= heappop(track)
+            if additionalRocks >= 0:
+                count += 1  
+        return count        
