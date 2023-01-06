76ae2750d49cafbc91a5ffeb6f88d6391a3e40c9
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2023-01-06 16:45:26 +0300

    Time: 968 ms (56.49%), Space: 21.5 MB (76.25%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -25,34 +25,4 @@ class Solution:
                 self.hascycle(i,graph,colors)
         
         return [i for i in range(n) if colors[i]==2]
-        
-        
-        
-# class Solution:
-#     def hascycle(self,cur_node,graph,color): 
-#         if color[cur_node] == 1:
-#             return True
-#         if color[cur_node] == 2:
-#             return False
-#         color[cur_node] = 1
-#         for nb in graph[cur_node]:
-#             cfound = self.hascycle(nb,graph,color)
-#             if cfound :
-#                 return True
-        
-#         color[cur_node] = 2
-#         return False
-#     def canFinish(self, numCourses: int, preq: List[List[int]]) -> bool:
-#         graph = defaultdict(list)
-        
-        
-#         for ai,bi in preq:
-#             graph[bi].append(ai)
-            
-#         colors = [0]*numCourses
-#         for i in range(numCourses):
-#             if colors[i] != 2:
-#                 cycle_found = self.hascycle(i,graph,colors)
-#                 if cycle_found:
-#                     return False
-#         return True
+    
