eef788413021561355938743e5604626b91102aa
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2023-01-10 19:57:11 +0300

    Time: 1415 ms (76.64%), Space: 129.5 MB (55.45%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -23,18 +23,5 @@ class Solution:
         for i in range(n):
             if colors[i] != 2:
                 cycle_found = self.hascycle(edges,i,colors,1,ans)
-        print(ans,colors)
+    
         return max(ans) if ans else -1
-#      def hascycle(self,cur_node,graph,color): 
-#         if color[cur_node] == 1:
-#             return True
-#         if color[cur_node] == 2:
-#             return False
-#         color[cur_node] = 1
-#         for nb in graph[cur_node]:
-#             cfound = self.hascycle(nb,graph,color)
-#             if cfound :
-#                 return True
-        
-#         color[cur_node] = 2
-#         return False
