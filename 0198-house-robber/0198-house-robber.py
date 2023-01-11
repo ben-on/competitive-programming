324a70e7396a1149d44384004bf10ec6b2db5f1a
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2023-01-11 10:44:22 +0300

    Time: 38 ms (53.97%), Space: 14.2 MB (16.41%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -2,9 +2,52 @@ class Solution:
     dp={}
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        @cache
+        @lru_cache
         def dfs(idx):
             if idx >= n:
                 return 0
-            return max(nums[idx]+dfs(idx+2),dfs(idx+1))
+            return max(dfs(idx+1),nums[idx]+dfs(idx+2))
         return dfs(0)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # n = len(nums)
+        # @cache
+        # def dfs(idx):
+        #     if idx >= n:
+        #         return 0
+        #     return max(nums[idx]+dfs(idx+2),dfs(idx+1))
+        # return dfs(0)
