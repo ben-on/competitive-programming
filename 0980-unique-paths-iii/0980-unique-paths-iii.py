c922ad0ec9846507d03966ca29842eb1617125ac
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2023-01-13 18:27:53 +0300

    Time: 48 ms (89.98%), Space: 13.9 MB (86.48%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -3,21 +3,21 @@ class Solution:
 
         M, N = range(len(grid)), range(len(grid[0]))
 
-        zeros = sum(row.count(0) for row in grid)       # count the zeros to ensure all cells visited
-        start = tuple((r,c) for r in M for c in N       # find start in grid
+        zeros = sum(row.count(0) for row in grid)
+        start = tuple((r,c) for r in M for c in N 
                            if grid[r][c] == 1)[0]
         self.ans = 0
 
         def dfs(row, col, zeros):
-            grid[row][col] = 3                          # change 0 to 3 to avoid returning
+            grid[row][col] = 3
 
-            for dr, dc in ((-1,0),(0,-1),(1,0),(0,1)):  # explore the grid recursively
+            for dr, dc in ((-1,0),(0,-1),(1,0),(0,1)): 
                 R, C = row+dr, col+dc
                 if R in M and C in N:
                     if grid[R][C] == 0: dfs(R, C, zeros-1)
                     if grid[R][C] == 2 and zeros == 0: self.ans += 1
 
-            grid[row][col] = 0                          # change back
+            grid[row][col] = 0
             return
 
         dfs(*start, zeros)
