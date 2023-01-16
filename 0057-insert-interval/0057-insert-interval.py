0c49baa71a6af4976828540b9bdefb62db194272
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2023-01-17 00:49:48 +0300

    Time: 94 ms (51.70%), Space: 17.5 MB (16.53%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,13 +1,12 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        
         heapq.heapify(intervals)
         heapq.heappush(intervals,newInterval)
         ans = [heapq.heappop(intervals)]
         while intervals:
             temp = heapq.heappop(intervals)
             if temp[0] <= ans[-1][1]:
-                ans[-1][0] = min(temp[0],ans[-1][0])
+                # ans[-1][0] = min(temp[0],ans[-1][0])
                 ans[-1][1] = max(temp[1],ans[-1][1])
             else:
                 ans.append(temp)
