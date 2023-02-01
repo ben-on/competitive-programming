f1bc96eebfde17758d9e71e78e31ec2c43c9e9d1
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2023-02-01 22:37:03 +0300

    Time: 3636 ms (20.98%), Space: 14.3 MB (51.75%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -1,16 +1,13 @@
 class Solution:
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
-        
-        score_ages = list(sorted((a,s) for s,a in zip(scores,ages)))
-        
-        
-        dp = [score_ages[i][1] for i in range(len(score_ages))]
-        for i in range(1,len(score_ages)):
+        agesSc = list(sorted((a,s) for s,a in zip(scores,ages)))
+        dp = [agesSc[i][1] for i in range(len(agesSc))]
+        for i in range(1,len(agesSc)):
             for j in range(i):
-                if score_ages[j][0] == score_ages[i][0]:
+                if agesSc[j][0] == agesSc[i][0]:
                     
-                    dp[i] = max(dp[i],dp[j]+score_ages[i][1])
-                elif score_ages[j][0] != score_ages[i][0]:
-                    if score_ages[i][1] >= score_ages[j][1]:
-                        dp[i] = max(dp[i],dp[j]+score_ages[i][1])
+                    dp[i] = max(dp[i],dp[j]+agesSc[i][1])
+                elif agesSc[j][0] != agesSc[i][0]:
+                    if agesSc[i][1] >= agesSc[j][1]:
+                        dp[i] = max(dp[i],dp[j]+agesSc[i][1])
         return max(dp)
