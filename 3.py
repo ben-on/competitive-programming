a01c55102de56f5de2300133faa6e9de64df8dee
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2023-02-14 00:21:32 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,1 +1,11 @@
-pass
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        dic = {0:1}
+        pre = 0
+        ans = 0
+        for val in nums:
+            pre +=val
+            ans += dic.get(pre%k,0)
+            dic[pre%k] = dic.get(pre%k,0) + 1
+        return ans
+            
