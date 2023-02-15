5ee8e61b646a68dfb9f4161202d510f19202f2e0
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2023-02-15 23:37:03 +0300

    Update squid31.py

--- squid31.py
+++ squid31.py
@@ -1,1 +1,6 @@
-pass
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        num=[str(i) for i in num]
+        s=list(str(int("".join(num)) + k))
+        return [int(i) for i in s]
+        
