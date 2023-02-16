37dcaba906581a76c5553e77b9cba960fc21a6b4
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2023-02-16 15:19:37 +0300

    Update squid32.py

--- squid32.py
+++ squid32.py
@@ -1,1 +1,43 @@
-pass
+# Below is the interface for Iterator, which is already defined for you.
+#
+# class Iterator:
+#     def __init__(self, nums):
+#         """
+#         Initializes an iterator object to the beginning of a list.
+#         :type nums: List[int]
+#         """
+#
+#     def hasNext(self):
+#         """
+#         Returns true if the iteration has more elements.
+#         :rtype: bool
+#         """
+#
+#     def next(self):
+#         """
+#         Returns the next element in the iteration.
+#         :rtype: int
+#         """
+
+class PeekingIterator:
+    def __init__(self, iterator):
+        self.ptr = 0
+        self.arr = iterator.v
+    
+    def peek(self):
+        return self.arr[self.ptr]
+        
+    def next(self):
+        next_element = self.arr[self.ptr]
+        self.ptr += 1
+        return next_element
+
+    def hasNext(self):
+        return self.ptr < len(self.arr)
+        
+
+# Your PeekingIterator object will be instantiated and called as such:
+# iter = PeekingIterator(Iterator(nums))
+# while iter.hasNext():
+#     val = iter.peek()   # Get the next element but not advance the iterator.
+#     iter.next()         # Should return the same value as [val].
