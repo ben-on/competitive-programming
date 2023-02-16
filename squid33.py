fb5a95e8373bcddd0fc255527e0724d436e18e27
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2023-02-16 15:20:38 +0300

    Update squid33.py

--- squid33.py
+++ squid33.py
@@ -1,1 +1,21 @@
-pass
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        stk = []
+        excuse = 0
+        for i in s:
+            if i == ')':
+                
+                if excuse:
+                    excuse-=1
+                else:
+                    if stk:
+                        stk.pop()
+                    else:
+                        return False
+                     
+            elif i == "(":
+                stk.append(i)
+            else:
+                excuse+=1
+        return  len(stk)<=excuse
+                
