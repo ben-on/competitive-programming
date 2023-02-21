d605e0d8ad8f8db176c77d7c8f55cf7773993116
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2023-02-21 09:09:02 +0300

    Update squid413.py

--- squid413.py
+++ squid413.py
@@ -1,16 +1,13 @@
 class Solution:
-    def validIPAddress(self, IP):
-
-        def isIPv4(s):
+    def validIPAddress(self, queryIP: str) -> str:
+        def checkV4(s):
             try: return str(int(s)) == s and 0 <= int(s) <= 255
             except: return False
-
-        def isIPv6(s):
+        def checkV6(s):
             try: return len(s) <= 4 and int(s, 16) >= 0
             except: return False
-
-        if IP.count(".") == 3 and all(isIPv4(i) for i in IP.split(".")):
+        if queryIP.count(".") == 3 and all(checkV4(i) for i in queryIP.split(".")):
             return "IPv4"
-        if IP.count(":") == 7 and all(isIPv6(i) for i in IP.split(":")):
+        if queryIP.count(":") == 7 and all(checkV6(i) for i in queryIP.split(":")):
             return "IPv6"
         return "Neither"   
