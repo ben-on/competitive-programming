4c30d867fa9b089ececb8f342d543792eaf54795
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2023-02-21 08:58:55 +0300

    Update squid412.py

--- squid412.py
+++ squid412.py
@@ -1,1 +1,9 @@
-pass
+class Solution:
+    def expressiveWords(self, S, words):
+        def check(S,W):
+            i, j, n, m = 0, 0, len(S), len(W)
+            for i in range(n):
+                if j < m and S[i] == W[j]: j += 1
+                elif S[i - 1:i + 2] != S[i] * 3 != S[i - 2:i + 1]: return False
+            return j == m
+        return sum(check(S, W) for W in words)
