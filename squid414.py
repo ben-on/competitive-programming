be968332d706d7bf1440eb9aa363cd43702273a5
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2023-02-21 18:58:37 +0300

    Update squid414.py

--- squid414.py
+++ squid414.py
@@ -1,1 +1,17 @@
-pass
+class Solution:
+    def dfs(self,target,stickers):
+        if not target: 
+            return 0
+        if target in self.map: 
+            return self.map[target]
+        cnt, res = collections.Counter(target), float('inf')
+        for c in stickers: 
+            if c[target[0]] == 0: continue
+            nxt = self.dfs(''.join([s * t for (s, t) in (cnt - c).items()]),stickers)
+            if nxt != -1: res = min(res, 1 + nxt)
+        self.map[target] = -1 if res == float('inf') else res
+        return self.map[target]
+    def minStickers(self, stickers: 'List[str]', target: 'str') -> 'int':
+        stickers, self.map = [collections.Counter(s) for s in stickers if set(s) & set(target)], {}
+        
+        return self.dfs(target,stickers)
