0a7b24953510c0d56a4b5953727ef0e1e8371b86
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2024-08-22 21:34:04 +0300

    Time: 56 ms (95.73%), Space: 17.7 MB (61.5%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.py
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -1,18 +1,26 @@
 class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
         n, m = len(dungeon), len(dungeon[0])
-        dungeon[-1][-1] = min(dungeon[-1][-1], 0)
-        for i in range(n - 1, -1,-1):
-            for j in range(m - 1, -1 , -1):
-                if (i,j) == (n - 1,m - 1):
+        dungeon[-1][-1] = min(0, dungeon[-1][-1])
+
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1 , -1 , -1):
+                if (i,j) == (n - 1, m - 1):
                     continue
-                down = dungeon[i + 1][j] if  i + 1 < n else -float('inf')
-                right = dungeon[i][j + 1] if  j + 1 < m else -float('inf')
                 
-                dungeon[i][j] = min(0, dungeon[i][j] + max(down, right))
+                right = dungeon[i][j + 1] if j + 1 < m else -float('inf')
+                down = dungeon[i + 1][j] if i + 1 < n else -float('inf')
+
+                dungeon[i][j] = min(0, dungeon[i][j] + max(right, down))
+        
+        
+        return  1 - (dungeon[0][0])
 
 
-        return abs(dungeon[0][0]) + 1
+
+
+
+        
 
 
 
