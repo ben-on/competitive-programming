4ca0f85f848e662e896df84d96e2e3d4ed551c6c
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2024-12-28 18:42:17 +0300

    Time: 89 ms (75.81%), Space: 18.3 MB (7.48%) - LeetHub

--- 1374-leftmost-column-with-at-least-a-one/1374-leftmost-column-with-at-least-a-one.py
+++ 1374-leftmost-column-with-at-least-a-one/1374-leftmost-column-with-at-least-a-one.py
@@ -2,20 +2,34 @@
 # This is BinaryMatrix's API interface.
 # You should not implement it, or speculate about its implementation
 # """
-#class BinaryMatrix(object):
+#class BinaryMatrix():
+#    def __init__(mat, row, col):
+#       self.mat = mat
+#       self.row = row
+#       self.col = col   
 #    def get(self, row: int, col: int) -> int:
+#        return self.mat[row][col]
 #    def dimensions(self) -> list[]:
+ #       return [self.row, self.col]
+
+
 
 class Solution:
-    def leftMostColumnWithOne(self, mat: 'BinaryMatrix') -> int:
-        rows,cols = mat.dimensions()
-        minCol = float('inf')
-        col = cols-1
-        for row in range(rows):
-            while col >= 0:
-                if mat.get(row,col) == 0:
-                    break
-                if mat.get(row,col) == 1:
-                    minCol = min(minCol,col)
-                col -= 1
-        return minCol if minCol != float('inf') else -1
+    def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
+        row, col = binaryMatrix.dimensions()
+        ans = 101
+        
+        for i in range(row):
+            l, r = 0, col - 1
+            
+            while l <= r:
+                mid = (l + r) // 2
+                val = binaryMatrix.get(i, mid)
+                
+                if val == 1:
+                    ans = min(ans, mid)
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        
+        return ans if ans != 101 else -1
