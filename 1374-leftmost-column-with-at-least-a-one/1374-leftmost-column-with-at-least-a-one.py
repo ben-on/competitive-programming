bc64acd1d49be8f407ce84ebc1ce0d0e093a5684
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2025-01-09 20:41:36 +0300

    Time: 93 ms (57.46%), Space: 18.2 MB (20%) - LeetHub

--- 1374-leftmost-column-with-at-least-a-one/1374-leftmost-column-with-at-least-a-one.py
+++ 1374-leftmost-column-with-at-least-a-one/1374-leftmost-column-with-at-least-a-one.py
@@ -8,20 +8,11 @@
 
 class Solution:
     def leftMostColumnWithOne(self, BinaryMatrix: 'BinaryMatrix') -> int:
-        def getFirstIndex(BinaryMatrix,rowIndex,m):
-            low,high=0,m-1
-            while low<=high:
-                mid=(low+high)>>1
-                if BinaryMatrix.get(rowIndex, mid)==0:
-                    low=mid+1
-                else:
-                    high=mid-1
-            return low if low<m else float("inf")
-
-        def solve(BinaryMatrix):
-            ans=float("inf")
-            n,m=BinaryMatrix.dimensions()
-            for i in range(n):
-                ans=min(ans,getFirstIndex(BinaryMatrix,i,m))
-            return ans if ans!=float("inf") else -1
-        return solve(BinaryMatrix)
+        n,m=BinaryMatrix.dimensions()
+        i,j=0,m-1
+        while i<n and j>=0:
+            if BinaryMatrix.get(i, j)==0:
+                i+=1
+            else:
+                j-=1
+        return j+1 if j<m-1 else -1
