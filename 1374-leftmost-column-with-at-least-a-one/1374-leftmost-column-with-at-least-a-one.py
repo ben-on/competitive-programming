25193958eadad18a6a6b597f73555d7acba35891
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2025-01-09 20:29:30 +0300

    Time: 91 ms (68.45%), Space: 18.1 MB (28.73%) - LeetHub

--- 1374-leftmost-column-with-at-least-a-one/1374-leftmost-column-with-at-least-a-one.py
+++ 1374-leftmost-column-with-at-least-a-one/1374-leftmost-column-with-at-least-a-one.py
@@ -2,34 +2,26 @@
 # This is BinaryMatrix's API interface.
 # You should not implement it, or speculate about its implementation
 # """
-#class BinaryMatrix():
-#    def __init__(mat, row, col):
-#       self.mat = mat
-#       self.row = row
-#       self.col = col   
+#class BinaryMatrix(object):
 #    def get(self, row: int, col: int) -> int:
-#        return self.mat[row][col]
 #    def dimensions(self) -> list[]:
- #       return [self.row, self.col]
-
-
 
 class Solution:
-    def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
-        row, col = binaryMatrix.dimensions()
-        ans = 101
-        
-        for i in range(row):
-            l, r = 0, col - 1
-            
-            while l <= r:
-                mid = (l + r) // 2
-                val = binaryMatrix.get(i, mid)
-                
-                if val == 1:
-                    ans = min(ans, mid)
-                    r = mid - 1
+    def leftMostColumnWithOne(self, BinaryMatrix: 'BinaryMatrix') -> int:
+        def getFirstIndex(BinaryMatrix,rowIndex,m):
+            low,high=0,m-1
+            while low<=high:
+                mid=(low+high)>>1
+                if BinaryMatrix.get(rowIndex, mid)==0:
+                    low=mid+1
                 else:
-                    l = mid + 1
-        
-        return ans if ans != 101 else -1
+                    high=mid-1
+            return low if low<m else float("inf")
+
+        def solve(BinaryMatrix):
+            ans=float("inf")
+            n,m=BinaryMatrix.dimensions()
+            for i in range(n):
+                ans=min(ans,getFirstIndex(BinaryMatrix,i,m))
+            return ans if ans!=float("inf") else -1
+        return solve(BinaryMatrix)
