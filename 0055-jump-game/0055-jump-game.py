675f18adf9b26ecc6848bc96770254a7d7dad9f6
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2025-02-15 11:59:35 +0300

    Time: 8607 ms (5%), Space: 20.9 MB (5.45%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,15 +1,21 @@
 class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        GOOD, BAD, UNKNOWN = 1, 0, -1
-        memo = [UNKNOWN] * len(nums)
-        memo[-1] = GOOD
-        for i in range(len(nums) - 2, -1, -1):
-            furthest_jump = min(i + nums[i], len(nums) - 1)
-            for j in range(i + 1, furthest_jump + 1):
-                if memo[j] == GOOD:
-                    memo[i] = GOOD
-                    break
-        return memo[0] == GOOD
+    def __init__(self):
+        self.memo = []
+        self.nums = []
 
+    def canJumpFromPosition(self, position):
+        if self.memo[position] != -1:
+            return self.memo[position] == 1
+        furthestJump = min(position + self.nums[position], len(self.nums) - 1)
+        for nextPosition in range(position + 1, furthestJump + 1):
+            if self.canJumpFromPosition(nextPosition):
+                self.memo[position] = 1
+                return True
+        self.memo[position] = 0
+        return False
 
-
+    def canJump(self, nums):
+        self.nums = nums
+        self.memo = [-1] * len(nums)  # -1 for unknown, 0 for bad, 1 for good
+        self.memo[-1] = 1  # The last position is always "good"
+        return self.canJumpFromPosition(0)
