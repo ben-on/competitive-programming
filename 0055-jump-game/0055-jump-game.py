cbf566fa5e587f314318758a3e5e4eb2e1ff53c2
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2025-02-15 12:08:38 +0300

    Time: 4272 ms (7.76%), Space: 18.8 MB (35.89%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,21 +1,14 @@
 class Solution:
-    def __init__(self):
-        self.memo = []
-        self.nums = []
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        memo = [False] * n
+        memo[-1] = True
 
-    def canJumpFromPosition(self, position):
-        if self.memo[position] != -1:
-            return self.memo[position] == 1
-        furthestJump = min(position + self.nums[position], len(self.nums) - 1)
-        for nextPosition in range(position + 1, furthestJump + 1):
-            if self.canJumpFromPosition(nextPosition):
-                self.memo[position] = 1
-                return True
-        self.memo[position] = 0
-        return False
-
-    def canJump(self, nums):
-        self.nums = nums
-        self.memo = [-1] * len(nums)  # -1 for unknown, 0 for bad, 1 for good
-        self.memo[-1] = 1  # The last position is always "good"
-        return self.canJumpFromPosition(0)
+        for i in range(n - 2, -1, -1):
+            
+            for j in range(i + 1, min(i + nums[i] + 1, n) ):
+                memo[i] = memo[i] or memo[j]
+                if memo[i] == True:
+                    break
+            
+        return memo[0]
