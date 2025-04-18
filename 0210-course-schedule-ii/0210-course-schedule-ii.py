f55c7af1435eaa3732d30e0937fbbd612a9352ea
BENIYAM-ALEMU <91593613+ben-on@users.noreply.github.com>
2025-04-18 18:23:38 +0300

    Time: 0 ms (100%), Space: 18.7 MB (99.43%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,48 +1,31 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = [[] for i in range(numCourses)]
-        pres = [0] * numCourses
+        # Build an adjacency list to represent the course dependency graph
+        course_graph = [[] for _ in range(numCourses)]
+        # Track the number of prerequisites for each course
+        prereq_count = [0] * numCourses
 
-        for course, pre in prerequisites:
-            graph[pre].append(course)
-            pres[course] += 1
-        
-        ans = deque([])
-        visited = [0] * numCourses
-        # 0 means not visited, 1 is gray, 2 is black
+        # Populate the graph and prereq_count list
+        for course, prereq in prerequisites:
+            course_graph[prereq].append(course)
+            prereq_count[course] += 1
 
-        def dfs(node):
-            if visited[node] == 2:
-                return True
+        # Initialize a queue with courses that have no prerequisites
+        queue = deque([i for i in range(numCourses) if prereq_count[i] == 0])
+        # This will store the final course order
+        course_order = []
 
-            if graph[node] == []:
-                visited[node] = 2
-                ans.appendleft(node)
-                return True
+        while queue:
+            # Pop a course with no remaining prerequisites
+            current_course = queue.popleft()
+            course_order.append(current_course)
 
-            if visited[node] == 1:
-                return False
+            # Reduce the prereq count of its dependent courses
+            for dependent in course_graph[current_course]:
+                prereq_count[dependent] -= 1
+                # If a dependent course has no more prerequisites, add it to the queue
+                if prereq_count[dependent] == 0:
+                    queue.append(dependent)
 
-            visited[node] = 1
-            res = True
-            for child in graph[node]:
-                res = res and dfs(child)
-            
-            visited[node] = 2
-            ans.appendleft(node)
-
-            return res
-        
-        nocycle = True
-
-        for i in range(numCourses):
-            if visited[i] == 0:
-                nocycle = nocycle and dfs(i)
-        
-        if nocycle:
-            return list(ans)
-        
-        return []
-                
-
-            
+        # If all courses are accounted for, return the valid order; else return empty list
+        return course_order if len(course_order) == numCourses else []
